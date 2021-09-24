fb36930285323af41c47e943c4b9d2bde72f12bf
arie kiswanto <arie.kiswanto12@gmail.com>
2021-09-24 23:37:53 +0700

    updated readme n fix app unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { PatientService } from './core/services/patient.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent
       ],
+      providers: [PatientService]
     }).compileComponents();
   });
 
@@ -20,5 +25,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  
+
 });
