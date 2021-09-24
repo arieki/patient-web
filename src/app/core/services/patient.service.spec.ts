5b36b27a3894e6a6db85ceadcb0c6c1dcf14afd0
arie kiswanto <arie.kiswanto12@gmail.com>
2021-09-24 21:42:16 +0700

    added patient service

--- src/app/core/services/patient.service.spec.ts
+++ src/app/core/services/patient.service.spec.ts
@@ -1,16 +1,80 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PatientService } from './patient.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('PatientService', () => {
   let service: PatientService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PatientService]
+    });
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(PatientService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should invoke save endpoint', () => {
+    service.save({}).subscribe(resp => {
+      expect(resp).toEqual({});
+    })
+
+    const dataAPI = `${service.saveUrl}`;
+    const req = httpTestingController.expectOne(`${dataAPI}`);
+
+    expect(req.request.method).toEqual('POST');
+
+    req.flush({});
+  });
+
+  it('should invoke update endpoint', () => {
+    service.update('any-id', {}).subscribe(resp => {
+      expect(resp).toEqual({});
+    })
+
+    const dataAPI = `${service.updateUrl}any-id`;
+    const req = httpTestingController.expectOne(`${dataAPI}`);
+
+    expect(req.request.method).toEqual('PUT');
+
+    req.flush({});
+  });
+
+  it('should invoke fetch patients by page endpoint', () => {
+    service.findNextData(0, 3).subscribe(resp => {
+      expect(resp).toEqual({});
+    })
+
+    const dataAPI = `${service.getAllByPagingUrl}?page=0&size=3`;
+    const req = httpTestingController.expectOne(`${dataAPI}`);
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush({});
+  });
+
+  it('shoudl invoke delete endpoint', () => {
+    service.delete('1').subscribe(resp => {
+      expect(resp).toEqual({});
+    })
+
+    const dataAPI = `${service.deleteUrl}?id=1`;
+    const req = httpTestingController.expectOne(`${dataAPI}`);
+
+    expect(req.request.method).toEqual('DELETE');
+
+    req.flush({});
+  });
+
+
 });
--- src/app/core/services/patient.service.ts
+++ src/app/core/services/patient.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
 
-  constructor() { }
+  saveUrl = environment.apiUrl + "/api/v1/save";
+  updateUrl = environment.apiUrl + "/api/v1/update/";
+  getAllByPagingUrl = environment.apiUrl + "/api/v1/patients";
+  deleteUrl = environment.apiUrl + "/api/v1/delete";
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  save(patient: any): Observable<any> {
+    return this.http.post(this.saveUrl, JSON.stringify(patient));
+  }
+
+  update(id: string, patient: any): Observable<any> {
+    return this.http.put(this.updateUrl + id, JSON.stringify(patient))
+  }
+
+  findNextData(page: any, size: any): Observable<any> {
+    let param: HttpParams = new HttpParams();
+    param = param.append('page', page);
+    param = param.append('size', size);
+    return this.http.get(this.getAllByPagingUrl, {params: param});
+  }
+
+  delete(id: any): Observable<any> {
+    let param: HttpParams = new HttpParams();
+    param = param.append('id', id);
+    return this.http.delete(this.deleteUrl, {params: param});
+  }
 }
